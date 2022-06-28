57939e58bf9bc636b0e3371fe6e115f59717c738
ZeeshanNSP <zeeshan20231@outlook.com>
2022-06-28 14:33:48 +0400

    280622

---
+++ NV200_Dashboard/Pages/terminal.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NV200_Dashboard.Pages
+{
+    public class terminalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
